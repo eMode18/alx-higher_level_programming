cd939369a8dd4f66ffb56f48819dd71510e85ab3
eMode18 <ewagandi@gmail.com>
2023-08-09 12:08:49 -0700

    lists.h correction

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -17,5 +17,8 @@ typedef struct listint_r
 } listint_t;
 
 int check_cycle(listint_t *list);
+void free_listint(listint_t *head);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
