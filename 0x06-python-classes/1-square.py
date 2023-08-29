9621871eb8e535b2b087c246a54fcc1ec25bad84
eMode18 <ewagandi@gmail.com>
2023-08-29 07:26:44 -0700

    correction task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
+""""define a class called square"""
+
 class Square:
     """This class represents a square"""
 
     def __init__(self, size):
+
+        """init class
+
+        Args:
+            size: new square size, type int
+        """
+
         self.__size = size
