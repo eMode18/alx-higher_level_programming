32117e87b574fa100c51f070fe342b57e8b6d55c
eMode18 <ewagandi@gmail.com>
2023-08-30 12:22:06 -0700

    task corrections

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print(my_list[i], end="")
-        tally += 1
+            tally += 1
         except IndexError:
             break
     print("")
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,6 +3,7 @@ def safe_print_list_integers(my_list=[], x=0):
     tally = 0
     for i in range(x):
         try:
+            if isinstance(my_list[i], int):
             print("{:d}".format(my_list[i]), end="")
             tally += 1
         except (ValueError, TypeError):
