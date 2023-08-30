375e8be7b89dfe01468db903d09070eb470e5ea7
eMode18 <ewagandi@gmail.com>
2023-08-30 01:08:58 -0700

    task 0 correction

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+     """x list elements to be printed
+
+    Args:
+        my_list (list): a list containing elements to print
+        x (int): elements to print
+
+    Returns:
+        total elemnts to print
+    """
     try:
         tally = 0
         for ele in my_list:
