7abb06e0cfeb40c3fb25f529fb9ea05141c522bb
eMode18 <ewagandi@gmail.com>
2023-09-05 02:05:16 -0700

    task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     @property
     def width(self):
         """Getter method for width"""
-        return self.__width
+        return (self.__width)
 
     @width.setter
     def width(self, value):
@@ -27,12 +27,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        (self.__width = value)
 
     @property
     def height(self):
         """Getter method for height"""
-        return self.__height
+        return (self.__height)
 
     @height.setter
     def height(self, value):
@@ -43,23 +43,23 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    def area(self) -> int:
+    def area(self):
         """Calculate and return the area of the rectangle"""
-        return self.__width * self.__height
+        return (self.__width * self.__height)
 
-    def perimeter(self) -> int:
+    def perimeter(self):
         """Calculate and return the perimeter of the rectangle"""
         if self.__width == 0 or self.__height == 0:
-            return 0
-        return 2 * (self.__width + self.__height)
+            return (0)
+        return (2 * (self.__width + self.__height))
 
     def __str__(self):
         """Provide a printable version of the rectangle, displaying it using the '#' character"""
         if self.__width == 0 or self.__height == 0:
-            return ""
+            return ("")
         rectObj = []
         for h in range(self.__height):
             [rectObj.append('#') for w in range(self.__width)]
             if h != self.__height - 1:
                 rectObj.append("\n")
-        return "".join(rectObj) + ("\n")
+        return ("".join(rectObj))
