c374d9c3370547f185496fb6045efdc2afd303c9
eMode18 <ewagandi@gmail.com>
2023-11-30 12:07:42 -0800

    task corrections

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # cURL headers task 4
 curl -sH "X-School-User-Id: 98" "${1}"
-
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,21 +9,26 @@ def find_peak(list_of_integers):
     Returns:
     - If exists, peak list element, otherwise None.
     """
-
-    if list_of_integers is None or list_of_integers == []:
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
-    least = 0
+
+    smallest = 0
     is_peak = len(list_of_integers)
-    mid = ((is_peak - least) // 2) + least
-    mid = int(mid)
-    if is_peak == 1:
-        return list_of_integers[0]
-    if is_peak == 2:
-        return max(list_of_integers)
-    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-            list_of_integers[mid] >= list_of_integers[mid + 1]:
-        return list_of_integers[mid]
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid + 1]:
-        return find_peak(list_of_integers[mid:])
-    if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
+
+    while smallest <= is_peak:
+        mid_value = (smallest + is_peak) // 2
+
+        if (mid_value == 0 or list_of_integers[mid_value]
+            >= list_of_integers[mid_value - 1]) and \
+           (mid_value == len(list_of_integers) - 1 or
+                list_of_integers[mid_value] >= list_of_integers[mid_value
+                                                                + 1]):
+            return list_of_integers[mid_value]
+
+        if mid_value < len(list_of_integers) - 1 and
+        list_of_integers[mid_value] < list_of_integers[mid_value + 1]:
+            smallest = mid_value + 1
+        else:
+            is_peak = mid_value - 1
+
+    return None
