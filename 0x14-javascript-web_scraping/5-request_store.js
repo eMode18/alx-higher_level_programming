5628941a794e8f4a896cbf0bf917e7d36e6e8a72
eMode18 <ewagandi@gmail.com>
2024-01-04 03:21:31 -0800

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,23 +1,4 @@
 #!/usr/bin/node
-
-const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  if (response.statusCode !== 200) {
-    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
-    return;
-  }
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log(`Content has been saved to ${filePath}`);
-  });
-});
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
