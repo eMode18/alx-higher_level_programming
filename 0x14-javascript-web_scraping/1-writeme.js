3da62f8fc399b0f248e71e075c9f3ca30f27e90b
eMode18 <ewagandi@gmail.com>
2024-01-04 03:44:00 -0800

    fixes

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
+const fileSystem = require('fs');
+
+const targetFile = process.argv[2];
+const fileContent = process.argv[3];
+
+fileSystem.writeFile(targetFile, fileContent, 'utf-8', function (error) {
+  if (error) {
+    console.log(error);
   }
-  console.log(`Content has been written to ${filePath}`);
 });
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,7 +7,8 @@ const url = process.argv[2];
 request(url, (error, response) => {
   if (error) {
     console.error(error);
-  } else {
-    console.log(`Status code: ${response.statusCode}`);
+    return;
   }
+  
+  console.log(`code: ${response.statusCode}`);
 });
